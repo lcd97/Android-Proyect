3d4ac2b0b55b3d5944817db0939c943b926ff54d
lcd97 <danycordero9@gmail.com>
2020-04-02 10:38:46 -0600

    - Register GUI -

--- app/src/main/java/com/example/libraryapp/register.java
+++ app/src/main/java/com/example/libraryapp/register.java
@@ -6,33 +6,21 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class register extends AppCompatActivity {
 
     Button btnLogin;
-    TextView txtRegister;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_register);
 
         btnLogin = findViewById(R.id.button);
-        txtRegister = findViewById(R.id.textView2);
 
         btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
-            }
-        });
-
-        txtRegister.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent i = new Intent(MainActivity.this, register.class);
-                startActivity(i);
             }
         });
     }
